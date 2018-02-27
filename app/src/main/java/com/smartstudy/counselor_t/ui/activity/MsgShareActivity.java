d31ad8b6e4e6a7a97cf63ee8ab18afe11c6059a6
luoyongming <luoyongming@innobuddy.com>
2018-02-27 15:51:46 +0800

    新增转发dialog

--- app/src/main/java/com/smartstudy/counselor_t/ui/activity/MsgShareActivity.java
+++ app/src/main/java/com/smartstudy/counselor_t/ui/activity/MsgShareActivity.java
@@ -4,11 +4,12 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
@@ -16,18 +17,30 @@ import android.widget.ImageView;
 import com.smartstudy.counselor_t.R;
 import com.smartstudy.counselor_t.entity.ChatUserInfo;
 import com.smartstudy.counselor_t.handler.WeakHandler;
+import com.smartstudy.counselor_t.listener.OnSendMsgDialogClickListener;
 import com.smartstudy.counselor_t.mvp.contract.MsgShareContract;
 import com.smartstudy.counselor_t.mvp.presenter.MsgSharePresenter;
 import com.smartstudy.counselor_t.ui.adapter.CommonAdapter;
+import com.smartstudy.counselor_t.ui.adapter.MultiItemTypeAdapter;
 import com.smartstudy.counselor_t.ui.adapter.base.ViewHolder;
 import com.smartstudy.counselor_t.ui.adapter.wrapper.EmptyWrapper;
 import com.smartstudy.counselor_t.ui.adapter.wrapper.HeaderAndFooterWrapper;
 import com.smartstudy.counselor_t.ui.base.BaseActivity;
+import com.smartstudy.counselor_t.ui.dialog.AppBasicDialog;
+import com.smartstudy.counselor_t.ui.dialog.DialogCreator;
 import com.smartstudy.counselor_t.util.DisplayImageUtils;
 import com.smartstudy.counselor_t.util.ParameterUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import io.rong.imkit.RongIM;
+import io.rong.imlib.IRongCallback;
+import io.rong.imlib.RongIMClient;
+import io.rong.imlib.model.Conversation;
+import io.rong.imlib.model.Message;
+import io.rong.imlib.model.MessageContent;
+import io.rong.message.TextMessage;
 
 public class MsgShareActivity extends BaseActivity<MsgShareContract.Presenter> implements MsgShareContract.View {
 
@@ -40,6 +53,7 @@ public class MsgShareActivity extends BaseActivity<MsgShareContract.Presenter> i
 
     private List<ChatUserInfo> chatUserInfoList;
     private WeakHandler mHandler;
+    private io.rong.imlib.model.Message sendMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +76,8 @@ public class MsgShareActivity extends BaseActivity<MsgShareContract.Presenter> i
         emptyView = mInflater.inflate(R.layout.layout_empty, rclv_recent, false);
         presenter.showLoading(this, emptyView);
         presenter.getChatUsers();
+        sendMsg = getIntent().getParcelableExtra("msg");
+        Log.d("msg====", sendMsg.toString());
     }
 
     private void initAdapter() {
@@ -87,7 +103,7 @@ public class MsgShareActivity extends BaseActivity<MsgShareContract.Presenter> i
         super.initEvent();
         mHandler = new WeakHandler(new Handler.Callback() {
             @Override
-            public boolean handleMessage(Message msg) {
+            public boolean handleMessage(android.os.Message msg) {
                 switch (msg.what) {
                     case ParameterUtils.MSG_WHAT_REFRESH:
                         rclv_recent.scrollBy(0, searchView.getHeight());
@@ -115,6 +131,39 @@ public class MsgShareActivity extends BaseActivity<MsgShareContract.Presenter> i
                 }
             }
         });
+        chatUserAdapter.setOnItemClickListener(new MultiItemTypeAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, RecyclerView.ViewHolder holder, int position) {
+                //头部view占去一个position
+                final ChatUserInfo chatUserInfo = chatUserInfoList.get(position - 1);
+                if (chatUserInfo != null) {
+                    MessageContent messageContent = sendMsg.getContent();
+                    if (TextMessage.class.isAssignableFrom(messageContent.getClass())) {
+                        final String content = ((TextMessage) messageContent).getContent();
+                        final AppBasicDialog dialog = DialogCreator.createSendTextMsgDialog(MsgShareActivity.this, chatUserInfo.getAvatar(), chatUserInfo.getName(), content, new OnSendMsgDialogClickListener() {
+                            @Override
+                            public void onPositive(String word) {
+                                //转发内容
+                                sendTextMsg(chatUserInfo.getId(), chatUserInfo.getName(), content);
+                                if (!TextUtils.isEmpty(word)) {
+                                    //转发留言
+                                    sendTextMsg(chatUserInfo.getId(), chatUserInfo.getName(), word);
+                                }
+                            }
+
+                            @Override
+                            public void onNegative() {
+                            }
+                        });
+                    }
+                }
+            }
+
+            @Override
+            public boolean onItemLongClick(View view, RecyclerView.ViewHolder holder, int position) {
+                return false;
+            }
+        });
     }
 
     @Override
@@ -136,5 +185,28 @@ public class MsgShareActivity extends BaseActivity<MsgShareContract.Presenter> i
     public void showEmptyView(View view) {
         emptyWrapper.setEmptyView(view);
         emptyWrapper.notifyDataSetChanged();
+    }
+
+    private void sendTextMsg(final String userId, final String userName, String content) {
+        TextMessage myTextMessage = TextMessage.obtain(content);
+        Message myMessage = Message.obtain(userId, Conversation.ConversationType.PRIVATE, myTextMessage);
+        RongIM.getInstance().sendMessage(myMessage, null, null, new IRongCallback.ISendMessageCallback() {
+            @Override
+            public void onAttached(Message message) {
+                //消息本地数据库存储成功的回调
+            }
+
+            @Override
+            public void onSuccess(Message message) {
+                //消息通过网络发送成功的回调
+                RongIM.getInstance().startPrivateChat(MsgShareActivity.this, userId, userName);
+            }
+
+            @Override
+            public void onError(Message message, RongIMClient.ErrorCode errorCode) {
+                //消息发送失败的回调
+                showTip("消息发送失败！");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/smartstudy/counselor_t/ui/dialog/DialogCreator.java
@@ -0,0 +1,164 @@
+package com.smartstudy.counselor_t.ui.dialog;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.smartstudy.counselor_t.R;
+import com.smartstudy.counselor_t.listener.OnSendMsgDialogClickListener;
+import com.smartstudy.counselor_t.ui.activity.LoginActivity;
+import com.smartstudy.counselor_t.util.DisplayImageUtils;
+import com.smartstudy.counselor_t.util.ParameterUtils;
+import com.smartstudy.counselor_t.util.ScreenUtils;
+
+
+public class DialogCreator {
+
+    public static AppBasicDialog createBaseCustomDialog(Context context, String title, String text,
+                                                        View.OnClickListener onClickListener) {
+        AppBasicDialog dialog = new AppBasicDialog(context, R.style.appBasicDialog);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View v = inflater.inflate(R.layout.dialog_base, null);
+        dialog.setContentView(v);
+        TextView titleTv = (TextView) v.findViewById(R.id.dialog_base_title_tv);
+        TextView textTv = (TextView) v.findViewById(R.id.dialog_base_text_tv);
+        Button confirmBtn = (Button) v.findViewById(R.id.dialog_base_confirm_btn);
+        titleTv.setText(title);
+        textTv.setText(text);
+        confirmBtn.setOnClickListener(onClickListener);
+        dialog.setCancelable(false);
+        return dialog;
+    }
+
+    public static AppBasicDialog createAppBasicDialog(final Context context, String title, String msg_tip, String txt_ok,
+                                                      String txt_cancle, View.OnClickListener onClickListener) {
+        final AppBasicDialog dialog = new AppBasicDialog(context, R.style.appBasicDialog);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.app_basicdialog, null);
+        dialog.setContentView(view);
+        view.findViewById(R.id.dialog_edit).setVisibility(View.GONE);
+        TextView tv_title = (TextView) view.findViewById(R.id.dialog_title);
+        tv_title.setVisibility(View.VISIBLE);
+        tv_title.setText(title);
+        ((TextView) view.findViewById(R.id.dialog_info)).setText(msg_tip);
+        Button ok_btn = ((Button) view.findViewById(R.id.positive_btn));
+        ok_btn.setText(txt_ok);
+        ok_btn.setOnClickListener(onClickListener);
+
+        Button cancle_btn = ((Button) view.findViewById(R.id.negative_btn));
+        cancle_btn.setText(txt_cancle);
+        cancle_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        return dialog;
+    }
+
+    public static AppBasicDialog createSendTextMsgDialog(final Context context, String avatar, String name,
+                                                         String content, final OnSendMsgDialogClickListener onClickListener) {
+        final AppBasicDialog dialog = new AppBasicDialog(context, R.style.appBasicDialog);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.dialog_send_text, null);
+        dialog.setContentView(view);
+        DisplayImageUtils.displayPersonImage(context, avatar, (ImageView) view.findViewById(R.id.iv_avatar));
+        ((TextView) view.findViewById(R.id.tv_name)).setText(name);
+        ((TextView) view.findViewById(R.id.tv_content)).setText(content);
+        Button positive_btn = view.findViewById(R.id.positive_btn);
+        final EditText et_word = view.findViewById(R.id.dialog_edit);
+        positive_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickListener.onPositive(et_word.getText().toString());
+            }
+        });
+        view.findViewById(R.id.negative_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickListener.onNegative();
+                dialog.dismiss();
+            }
+        });
+        WindowManager.LayoutParams p = dialog.getWindow().getAttributes();
+        p.width = (int) (ScreenUtils.getScreenWidth() * 0.85);
+        dialog.getWindow().setAttributes(p);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+        return dialog;
+    }
+
+    /**
+     * 是否登录dialog
+     *
+     * @param context
+     */
+    public static void createLoginDialog(final Activity context) {
+        LayoutInflater factory = LayoutInflater.from(context);
+        final AppBasicDialog dialog = new AppBasicDialog(context, R.style.appBasicDialog);
+        final View view = factory.inflate(R.layout.app_basicdialog, null);
+        dialog.addContentView(view, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        view.findViewById(R.id.dialog_edit).setVisibility(View.GONE);
+        ((TextView) view.findViewById(R.id.dialog_title)).setText(R.string.login);
+        ((TextView) view.findViewById(R.id.dialog_info)).setText(R.string.login_now);
+        Button ok_btn = ((Button) view.findViewById(R.id.positive_btn));
+        ok_btn.setText(R.string.sure);
+        ok_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //登录页面
+                context.startActivityForResult(new Intent(context, LoginActivity.class), ParameterUtils.REQUEST_CODE_LOGIN);
+                dialog.dismiss();
+            }
+        });
+        Button cacle_btn = ((Button) view.findViewById(R.id.negative_btn));
+        cacle_btn.setText(R.string.cancle);
+        cacle_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        WindowManager.LayoutParams p = dialog.getWindow().getAttributes();
+        p.width = (int) (ScreenUtils.getScreenWidth() * 0.85);
+        dialog.getWindow().setAttributes(p);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+    }
+
+    public static void createWebviewDialog(WebView wv, final Context mContext) {
+        wv.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
+                // 构建一个Builder来显示网页中的alert对话框
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setTitle("title");
+                builder.setMessage(message);
+                builder.setPositiveButton(android.R.string.ok, new AlertDialog.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        result.confirm();
+                    }
+                });
+                builder.setCancelable(false);
+                builder.create();
+                builder.show();
+                return true;
+            }
+        });
+    }
+}
