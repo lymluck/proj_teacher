e633d02464f398d3f59d1f7d94298dadf1db8fb8
luoyongming <luoyongming@innobuddy.com>
2018-05-21 19:32:05 +0800

    禁止登录框滑动返回

--- app/src/main/java/com/smartstudy/counselor_t/ui/activity/ReloginActivity.java
+++ app/src/main/java/com/smartstudy/counselor_t/ui/activity/ReloginActivity.java
@@ -21,6 +21,8 @@ public class ReloginActivity extends BaseActivity<BasePresenter> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // 首页禁用滑动返回
+        setSlideable(false);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relogin);
         setHeadVisible(View.GONE);
